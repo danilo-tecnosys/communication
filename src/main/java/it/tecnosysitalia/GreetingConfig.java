16ccb52e3fa8d6d850069c0064b4e98bb3a66009
quarkusio <no-reply@quarkus.io>
2024-11-05 20:46:18 +0000

    Initial commit

---
+++ src/main/java/it/tecnosysitalia/GreetingConfig.java
@@ -0,0 +1,12 @@
+package it.tecnosysitalia;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
